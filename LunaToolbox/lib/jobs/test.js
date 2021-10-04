d4e37a2ef5c5f92331c74005a9c75d7a86b81790
Ilya Shablovsky <ilya.shablovsky@lunalabs.io>
2021-10-04 17:00:38 +0300

    [EN-2087] Fix linux chromium bug

--- LunaToolbox/lib/jobs/test.js
+++ LunaToolbox/lib/jobs/test.js
@@ -12,7 +12,9 @@ async function runTests() {
 
     return new Promise(( resolve, reject ) => {
         ( async () => {
-            const browser = await puppeteer.launch();
+            const browser = await puppeteer.launch({
+                args: ['--no-sandbox', '--disable-setuid-sandbox'],
+            });
             const page = await browser.newPage();
 
             await page.exposeFunction( 'onTestResult', ( details ) => {
