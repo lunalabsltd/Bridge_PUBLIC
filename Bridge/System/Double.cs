fcfb4d58cfa44831c799c1e33242ca74fdeff6b9
Alexandr Matveev <alxr.matveev@gmail.com>
2022-04-21 10:24:41 +0300

    [EN-2794] Codestyle

--- Bridge/System/Double.cs
+++ Bridge/System/Double.cs
@@ -101,6 +101,6 @@ namespace System
         public override extern int GetHashCode();
 
         [Bridge.Template("System.Double.tryParse4({s}, {style}, {provider}, {result})")]
-        public static extern bool TryParse( string s, Globalization.NumberStyles style, IFormatProvider provider, out double result);
+        public static extern bool TryParse(string s, Globalization.NumberStyles style, IFormatProvider provider, out double result);
     }
 }
